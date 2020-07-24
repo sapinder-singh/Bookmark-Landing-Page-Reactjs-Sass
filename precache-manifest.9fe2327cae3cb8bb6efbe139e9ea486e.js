e5491a9d317794b9e5349011a9f363156684485a
Sapinder <sappiethird@gmail.com>
2020-07-24 19:12:39 +0530

    Updates

---
+++ precache-manifest.9fe2327cae3cb8bb6efbe139e9ea486e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0da0cea8f296209a90c1513b4fee8ca",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/index.html"
+  },
+  {
+    "revision": "49a6eaa52f02674af9e4",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/css/main.1d9c0e92.chunk.css"
+  },
+  {
+    "revision": "4fe8e5c16c3cac4ac7ad",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/js/2.b9bd4f38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/js/2.b9bd4f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49a6eaa52f02674af9e4",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/js/main.ac095e62.chunk.js"
+  },
+  {
+    "revision": "db3d698c8394d75a7fbf",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/js/runtime-main.6b172663.js"
+  },
+  {
+    "revision": "7c66c472eb50c9abe027977b6f2b406e",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/bg-dots.7c66c472.svg"
+  },
+  {
+    "revision": "38993f94ba58120b39289a5258af7063",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/icon-arrow.38993f94.svg"
+  },
+  {
+    "revision": "3f810892db938a420bc89b43de4ae9bd",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/icon-close.3f810892.svg"
+  },
+  {
+    "revision": "ba45f6788c38e8a4fc487eab6847a365",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/icon-error.ba45f678.svg"
+  },
+  {
+    "revision": "c24c1904c0c62485f4200c3a49a45a27",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/icon-facebook.c24c1904.svg"
+  },
+  {
+    "revision": "e3defca190ae3f2aaeedefe79c125bd6",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/icon-hamburger.e3defca1.svg"
+  },
+  {
+    "revision": "a4daae10e6bd237b96908d16566647e2",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/icon-twitter.a4daae10.svg"
+  },
+  {
+    "revision": "25a237330ac7fab409f2b5833363ae97",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/illustration-features-tab-1.25a23733.svg"
+  },
+  {
+    "revision": "11b0ec333ed9d17690ada04979c489c0",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/illustration-features-tab-2.11b0ec33.svg"
+  },
+  {
+    "revision": "7f696303479b76b89e45fcf33b7aff4b",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/illustration-features-tab-3.7f696303.svg"
+  },
+  {
+    "revision": "dfa0cd821469019e297c8c27cfc0d475",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/illustration-hero.dfa0cd82.svg"
+  },
+  {
+    "revision": "618f33f9f1aa8412c10d732776a7ff51",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/logo-bookmark.618f33f9.svg"
+  },
+  {
+    "revision": "3e1ab00bc1a7182ca9297e73eaa98480",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/logo-chrome.3e1ab00b.svg"
+  },
+  {
+    "revision": "e5023c2b57490fa1d28da6dea398606b",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/logo-firefox.e5023c2b.svg"
+  },
+  {
+    "revision": "3d8f7bcc99ea84508c6a3380e0ae0878",
+    "url": "/Bookmark-Landing-Page-Reactjs-Sass/static/media/logo-opera.3d8f7bcc.svg"
+  }
+]);
